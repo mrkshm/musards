6749b89b39c7010df40245c680525c695762d23b
mrks <hey@mrks.me>
2022-08-31 19:44:46 +0200

    changing meta head

--- components/MetaHead.jsx
+++ components/MetaHead.jsx
@@ -6,7 +6,7 @@ export default function MetaHead({ pageTitle }) {
       <title>La Compagnie Musards{pageTitle ? ` - ${pageTitle}` : null}</title>
       <meta
         name="description"
-        content="Informations de contact de la Compagnie Musards, spécialisée dans la production et diffusion de concerts, ciné-concerts et spectacles."
+        content="La Cie MUSARDS est une association créée par le pianiste-compositeur Vadim Sher, dédiée à la production et la diffusion de ciné-concerts, concerts et spectacles."
       />
       <link rel="icon" href="/musards_logo.jpg" />
     </Head>
